e1208ab0eec80a2e23967a00b4f21cb8392cdb2c
abelsiqueira <nepper271@gmail.com>
2022-01-07 20:53:50 +0000

    Deploying to gh-pages from  @ 8ddf43dbd5e12c40d83f596cbd77e538205c10cd 🚀

--- assets/index/code/ex2.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-using LinearAlgebra
-norm(A * x - b), norm(x .- 1)
--- assets/literate/index_script.jl
+++ assets/literate/index_script.jl
@@ -1,16 +1,6 @@
 # This file was generated, do not modify it.
 
-A = rand(5, 5)
-b = A * ones(5)
-x = A \ b
+using SolverCore
 
-using LinearAlgebra
-norm(A * x - b), norm(x .- 1)
-
-using Plots
-
-x = rand(3)
-y = rand(3)
-plot(x, y)
-png(joinpath("__site/assets", "myplot")) # hide
+SolverCore.STATUSES
 
